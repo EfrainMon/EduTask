9f46434b8e339571a68f5c29b38adc21f79e565d
Efrain Montalvo <efrainmx2001@gmail.com>
2023-12-02 13:22:59 -0600

    Commit Inicial

---
+++ app/src/main/java/com/bandup/edutask/BaseDatosHelper.java
@@ -0,0 +1,173 @@
+package com.bandup.edutask;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class BaseDatosHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "BaseDatosHelper";
+    private static final String DB_NAME = "EduTaskDB";
+    private static final int DB_VERSION = 1;
+
+    // Nombres de las tablas
+    private static final String TABLE_MATERIA = "Materia";
+    private static final String TABLE_ALUMNO = "Alumno";
+    private static final String TABLE_ASIGNACION = "Asignacion";
+    private static final String TABLE_ALUMNO_MATERIA = "Alumno_Materia";
+    private static final String TABLE_ALUMNO_ASIGNACION = "Alumno_Asignacion";
+
+    // Columnas de la tabla Alumno_Materia
+    private static final String COL_ALUMNO_MATERIA_ID = "ID";
+    private static final String COL_ALUMNO_MATERIA_NUM_CONTROL = "NumControl";
+    private static final String COL_ALUMNO_MATERIA_CLAVE = "Clave";
+
+    // Columnas de la tabla Alumno_Asignacion
+    private static final String COL_ALUMNO_ASIGNACION_ID = "ID";
+    private static final String COL_ALUMNO_ASIGNACION_NUM_CONTROL = "NumControl";
+    private static final String COL_ALUMNO_ASIGNACION_ID_ASIGNACION = "IDAsignacion";
+
+    // Columnas de la tabla Alumno
+    private static final String COL_ALUMNO_ID = "ID";
+    private static final String COL_ALUMNO_NUM_CONTROL = "NumControl";
+    private static final String COL_ALUMNO_NOMBRE = "Nombre";
+    private static final String COL_ALUMNO_A_PATERNO = "aPaterno";
+    private static final String COL_ALUMNO_A_MATERNO = "aMaterno";
+    private static final String COL_ALUMNO_OCULTO = "Oculto";
+
+    // Columnas de la tabla Asignacion
+    private static final String COL_ASIGNACION_ID = "ID";
+    private static final String COL_ASIGNACION_FECHA = "Fecha";
+    private static final String COL_ASIGNACION_NOMBRE = "Nombre";
+    private static final String COL_ASIGNACION_REALIZADA = "Realizada";
+    private static final String COL_ASIGNACION_OCULTO = "Oculto";
+
+    // Columnas de la tabla Materia
+    private static final String COL_MATERIA_ID = "ID";
+    private static final String COL_MATERIA_CLAVE = "Clave";
+    private static final String COL_MATERIA_NOMBRE = "Nombre";
+    private static final String COL_MATERIA_OCULTO = "Oculto";
+
+    public BaseDatosHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Crear la tabla Alumno_Materia
+        String crearTablaAlumnoMateria = "CREATE TABLE IF NOT EXISTS " + TABLE_ALUMNO_MATERIA + " ("
+                + COL_ALUMNO_MATERIA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_ALUMNO_MATERIA_NUM_CONTROL + " TEXT, "
+                + COL_ALUMNO_MATERIA_CLAVE + " TEXT)";
+        db.execSQL(crearTablaAlumnoMateria);
+
+        // Crear la tabla Alumno_Asignacion
+        String crearTablaAlumnoAsignacion = "CREATE TABLE IF NOT EXISTS " + TABLE_ALUMNO_ASIGNACION + " ("
+                + COL_ALUMNO_ASIGNACION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_ALUMNO_ASIGNACION_NUM_CONTROL + " TEXT, "
+                + COL_ALUMNO_ASIGNACION_ID_ASIGNACION + " INTEGER)";
+        db.execSQL(crearTablaAlumnoAsignacion);
+
+        // Crear la tabla Alumno
+        String crearTablaAlumno = "CREATE TABLE IF NOT EXISTS " + TABLE_ALUMNO + " ("
+                + COL_ALUMNO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_ALUMNO_NUM_CONTROL + " TEXT, "
+                + COL_ALUMNO_NOMBRE + " TEXT, "
+                + COL_ALUMNO_A_PATERNO + " TEXT, "
+                + COL_ALUMNO_A_MATERNO + " TEXT, "
+                + COL_ALUMNO_OCULTO + " INTEGER)";
+        db.execSQL(crearTablaAlumno);
+
+        // Crear la tabla Asignacion
+        String crearTablaAsignacion = "CREATE TABLE IF NOT EXISTS " + TABLE_ASIGNACION + " ("
+                + COL_ASIGNACION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_ASIGNACION_FECHA + " TEXT, "
+                + COL_ASIGNACION_NOMBRE + " TEXT, "
+                + COL_ASIGNACION_REALIZADA + " INTEGER, "
+                + COL_ASIGNACION_OCULTO + " INTEGER)";
+        db.execSQL(crearTablaAsignacion);
+
+        // Crear la tabla Materia
+        String crearTablaMateria = "CREATE TABLE IF NOT EXISTS " + TABLE_MATERIA + " ("
+                + COL_MATERIA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_MATERIA_CLAVE + " TEXT, "
+                + COL_MATERIA_NOMBRE + " TEXT, "
+                + COL_MATERIA_OCULTO + " INTEGER)";
+        db.execSQL(crearTablaMateria);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Puedes implementar la lógica de actualización de la base de datos si es necesario
+        // Por ahora, simplemente borramos todas las tablas y las volvemos a crear
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALUMNO_MATERIA);
+        // ... (borrar las otras tablas)
+        onCreate(db);
+    }
+
+    // Puedes agregar métodos para insertar, actualizar y consultar datos en cada tabla según tus necesidades
+
+    // Ejemplo de método para agregar datos a la tabla Alumno
+    // Métodos para operaciones en la tabla Alumno
+    public boolean addAlumno(String numControl, String nombre, String aPaterno, String aMaterno, int oculto) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_ALUMNO_NUM_CONTROL, numControl);
+        contentValues.put(COL_ALUMNO_NOMBRE, nombre);
+        contentValues.put(COL_ALUMNO_A_PATERNO, aPaterno);
+        contentValues.put(COL_ALUMNO_A_MATERNO, aMaterno);
+        contentValues.put(COL_ALUMNO_OCULTO, oculto);
+
+        long resultado = db.insert(TABLE_ALUMNO, null, contentValues);
+
+        return resultado != -1;
+    }
+
+    public Cursor getAlumnos() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_ALUMNO;
+        return db.rawQuery(query, null);
+    }
+
+    // ... (otros métodos para otras operaciones en la tabla Alumno)
+
+    // Métodos para operaciones en la tabla Materia
+    public boolean addMateria(String clave, String nombre, int oculto) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_MATERIA_CLAVE, clave);
+        contentValues.put(COL_MATERIA_NOMBRE, nombre);
+        contentValues.put(COL_MATERIA_OCULTO, oculto);
+
+        long resultado = db.insert(TABLE_MATERIA, null, contentValues);
+
+        return resultado != -1;
+    }
+
+    public void deleteMateria(String nombre, String clave) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Actualiza el valor de Oculto a 1 en lugar de eliminar la fila
+        ContentValues values = new ContentValues();
+        values.put(COL_MATERIA_OCULTO, 1);
+
+        // Utiliza 'nombre' en lugar de 'id' en la siguiente línea
+        db.update(TABLE_MATERIA, values, COL_MATERIA_NOMBRE + " = ? AND " + COL_MATERIA_CLAVE + " = ?", new String[]{nombre, clave});
+
+        // Puedes añadir logs o mensajes aquí para indicar que la materia fue "eliminada"
+        Log.d(TAG, "deleteMateria: Materia con nombre " + nombre + " y clave " + clave + " oculta (Oculto = 1)");
+    }
+
+
+    public Cursor getMaterias() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_MATERIA;
+        return db.rawQuery(query, null);
+    }
+
+    // ... (otros métodos para otras operaciones en las demás tablas)
+}
---
+++ app/src/main/java/com/bandup/edutask/MainActivity.java
@@ -0,0 +1,121 @@
+package com.bandup.edutask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bandup.edutask.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    BaseDatosHelper miBaseDatosHelper;
+
+    private Button btnCrearMateria;
+    private Button btnEliminarMateria;
+    private EditText edtNombreMateria, edtClaveMateria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        miBaseDatosHelper = new BaseDatosHelper(this);
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Encuentra referencias a los elementos de diseño
+        btnCrearMateria = findViewById(R.id.btnCrearMateria);
+        btnEliminarMateria = findViewById(R.id.btnEliminarMateria);
+        edtNombreMateria = findViewById(R.id.edtNombreMateria);
+        edtClaveMateria = findViewById(R.id.edtClaveMateria);
+
+        // Configura el OnClickListener para el botón
+        btnCrearMateria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtiene los valores de los EditText
+                String nombreMateria = edtNombreMateria.getText().toString();
+                String claveMateria = edtClaveMateria.getText().toString();
+
+                // Verifica si los campos no están vacíos
+                if (!nombreMateria.isEmpty() && !claveMateria.isEmpty()) {
+                    // Agrega la materia a la base de datos
+                    boolean exito = miBaseDatosHelper.addMateria(claveMateria, nombreMateria, 0);
+
+                    // Verifica si se agregó correctamente
+                    if (exito) {
+                        // Puedes mostrar un mensaje de éxito o realizar otras acciones
+                        Toast.makeText(MainActivity.this, "Materia agregada correctamente", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Puedes mostrar un mensaje de error si la inserción falló
+                        Toast.makeText(MainActivity.this, "Error al agregar la materia", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // Muestra un mensaje si algún campo está vacío
+                    Toast.makeText(MainActivity.this, "Todos los campos deben ser completados", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnEliminarMateria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtiene los valores de los EditText
+                String nombreMateria = edtNombreMateria.getText().toString();
+                String claveMateria = edtClaveMateria.getText().toString();
+
+                // Llamada al método deleteMateria en tu actividad o fragmento
+                miBaseDatosHelper.deleteMateria(nombreMateria, claveMateria);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
